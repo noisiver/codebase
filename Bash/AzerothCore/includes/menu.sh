a22fb8f4c9e35a578bf3b54a7aa370280be84cfa
Revision <tkn963@gmail.com>
2021-09-21 18:08:40 +0200

    AzerothCore: Fixes
    Fixed an issue when enabling and disabling the Eluna module. In certain cases it would disable the module when not actually changing the value itself within the menu. This should, hopefully, no longer happen.
    
    Reduced the size of all available elements. It might not look as pleasing but I want to minimize the required space the menu takes up.

--- Bash/AzerothCore/includes/menu.sh
+++ Bash/AzerothCore/includes/menu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function main_menu
 {
-    SELECTION=$(whiptail --title "AzerothCore" --menu "Choose an option" 25 78 16 \
+    SELECTION=$(whiptail --title "AzerothCore" --menu "Choose an option" 11 50 0 \
     1 "Manage the source code" \
     2 "Manage the databases" \
     3 "Manage the configuration files" \
@@ -23,7 +23,7 @@ function main_menu
 
 function source_menu
 {
-    SELECTION=$(whiptail --title "Manage the source code" --menu "Choose an option" 25 78 16 \
+    SELECTION=$(whiptail --title "Manage the source code" --menu "Choose an option" 11 50 0 \
     1 "Download or update the source code" \
     2 "Manage the available modules" \
     3 "Compile the source into binaries" \
@@ -52,9 +52,9 @@ function compile_source_menu
     [ ! -f $CORE_DIRECTORY/bin/start.sh ] && AUTH=1 WORLD=1
 
     SELECTION=$(whiptail --title "Compile the source into binaries" --checklist \
-    "Select the parts to use for this compilation" 20 78 4 \
-    "Auth" "Set up and use the authserver" $AUTH \
-    "World" "Set up and use the worldserver" $WORLD \
+    "Select the parts to use for this compilation" 11 50 4 \
+    "Auth" "Enable the use of authserver" $AUTH \
+    "World" "Enable the use of worldserver" $WORLD \
     3>&1 1>&2 2>&3)
 
     if [[ $SELECTION ]]; then
@@ -73,16 +73,13 @@ function compile_source_menu
 function source_module_menu
 {
     SELECTION=$(whiptail --title "Available Modules" --checklist \
-    "Select the modules to use" 20 78 4 \
-    "Eluna" "A LUA engine allowing the use of scripts written in LUA" ${MODULE_ELUNA_ENABLED/true/ON} \
+    "Select the modules to use" 11 50 4 \
+    "Eluna" "Allows scripts written in LUA" ${MODULE_ELUNA_ENABLED/true/ON} \
     3>&1 1>&2 2>&3)
 
     if [[ $SELECTION ]]; then
         if [[ $SELECTION == *"Eluna"* ]]; then
             MODULE_ELUNA_ENABLED="true"
-            generate_settings
-        else
-            MODULE_ELUNA_ENABLED="false"
             generate_settings
         fi
     else
@@ -103,10 +100,16 @@ function database_menu
         MYSQL_DATABASE_AUTH=$(whiptail --inputbox "The name of the auth database" 8 60 $MYSQL_DATABASE_AUTH --title "Manage the databases" 3>&1 1>&2 2>&3)
         MYSQL_DATABASE_CHARACTERS=$(whiptail --inputbox "The name of the characters database" 8 60 $MYSQL_DATABASE_CHARACTERS --title "Manage the databases" 3>&1 1>&2 2>&3)
         MYSQL_DATABASE_WORLD=$(whiptail --inputbox "The name of the world database" 8 60 $MYSQL_DATABASE_WORLD --title "Manage the databases" 3>&1 1>&2 2>&3)
-        generate_settings
+
+        if [[ $MYSQL_HOSTNAME ]] && [[ $MYSQL_PORT ]] && [[ $MYSQL_USERNAME ]] && [[ $MYSQL_PASSWORD ]] && [[ $MYSQL_DATABASE_AUTH ]] && [[ $MYSQL_DATABASE_CHARACTERS ]] && [[ $MYSQL_DATABASE_WORLD ]]; then
+            generate_settings
+        else
+            whiptail --title "An error has occured" --msgbox "At least one of the defined values is invalid or empty" 7 58
+            main_menu
+        fi
     fi
 
-    SELECTION=$(whiptail --title "Manage the databases" --menu "Choose an option" 25 78 16 \
+    SELECTION=$(whiptail --title "Manage the databases" --menu "Choose an option" 11 50 0 \
     1 "Not yet determined options" \
     3>&1 1>&2 2>&3)
 
@@ -121,7 +124,7 @@ function database_menu
 
 function configuration_menu
 {
-    SELECTION=$(whiptail --title "Manage the configuration files" --menu "Choose an option" 25 78 16 \
+    SELECTION=$(whiptail --title "Manage the configuration files" --menu "Choose an option" 11 50 0 \
     1 "Not yet determined options" \
     3>&1 1>&2 2>&3)
 
@@ -137,7 +140,7 @@ function configuration_menu
 function binary_menu
 {
     if [[ -f $CORE_DIRECTORY/bin/start.sh ]] && [[ -f $CORE_DIRECTORY/bin/worldserver ]] && [[ -f $CORE_DIRECTORY/bin/authserver ]]; then
-        SELECTION=$(whiptail --title "Manage the compiled binaries" --menu "Choose an option" 25 78 16 \
+        SELECTION=$(whiptail --title "Manage the compiled binaries" --menu "Choose an option" 11 50 0 \
         1 "Not yet determined options" \
         3>&1 1>&2 2>&3)
 
@@ -149,7 +152,7 @@ function binary_menu
             main_menu
         fi
     else
-        whiptail --title "An error has occured" --msgbox "Unable to locate the required files. Perform a compilation first by managing the source code" 8 78
+        whiptail --title "An error has occured" --msgbox "Unable to locate the required files\n\nYou first need to compile the source code into binaries" 9 59
         main_menu
     fi
 }
