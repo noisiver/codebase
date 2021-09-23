8375210cfffca73bb58015358cd05ae4e84d5c08
Revision <tkn963@gmail.com>
2021-09-23 17:11:16 +0200

    AzerothCore: Database
    Added the base for importing and updating databases. I still have to decide what number does what and how to balance it for both the terminal and the menu.

--- Bash/AzerothCore/includes/database.sh
+++ Bash/AzerothCore/includes/database.sh
@@ -13,6 +13,66 @@ function import_database
     echo "user=\"$MYSQL_USERNAME\"" >> $MYSQL_CONFIG
     echo "password=\"$MYSQL_PASSWORD\"" >> $MYSQL_CONFIG
 
+    if [ $1 == 0 ]; then
+        if [ ! -z `mysql --defaults-extra-file=$MYSQL_CONFIG --skip-column-names -e "SHOW DATABASES LIKE '$MYSQL_DATABASE_AUTH'"` ]; then
+            if [ -d $CORE_DIRECTORY/data/sql/base/db_auth ]; then
+                for f in $CORE_DIRECTORY/data/sql/base/db_auth/*.sql; do
+                    if [ ! -z `mysql --defaults-extra-file=$MYSQL_CONFIG --skip-column-names $MYSQL_DATABASE_AUTH -e "SHOW TABLES LIKE '$(basename $f .sql)'"` ]; then
+                        echo -e "\e[0;33mSkipping "$(basename $f)"\e[0m"
+                        continue;
+                    fi
+
+                    echo -e "\e[0;33mImporting "$(basename $f)"\e[0m"
+                    mysql --defaults-extra-file=$MYSQL_CONFIG $MYSQL_DATABASE_AUTH < $f
+                    if [ $? -ne 0 ]; then
+                        rm -rf $MYSQL_CONFIG
+                        exit $?
+                    fi
+                done
+            fi
+        fi
+    fi
+
+    if [ $1 == 0 ]; then
+        if [ ! -z `mysql --defaults-extra-file=$MYSQL_CONFIG --skip-column-names -e "SHOW DATABASES LIKE '$MYSQL_DATABASE_CHARACTERS'"` ]; then
+            if [ -d $CORE_DIRECTORY/data/sql/base/db_characters ]; then
+                for f in $CORE_DIRECTORY/data/sql/base/db_characters/*.sql; do
+                    if [ ! -z `mysql --defaults-extra-file=$MYSQL_CONFIG --skip-column-names $MYSQL_DATABASE_CHARACTERS -e "SHOW TABLES LIKE '$(basename $f .sql)'"` ]; then
+                        echo -e "\e[0;33mSkipping "$(basename $f)"\e[0m"
+                        continue;
+                    fi
+
+                    echo -e "\e[0;33mImporting "$(basename $f)"\e[0m"
+                    mysql --defaults-extra-file=$MYSQL_CONFIG $MYSQL_DATABASE_CHARACTERS < $f
+                    if [ $? -ne 0 ]; then
+                        rm -rf $MYSQL_CONFIG
+                        exit $?
+                    fi
+                done
+            fi
+        fi
+    fi
+
+    if [ $1 == 0 ]; then
+        if [ ! -z `mysql --defaults-extra-file=$MYSQL_CONFIG --skip-column-names -e "SHOW DATABASES LIKE '$MYSQL_DATABASE_WORLD'"` ]; then
+            if [ -d $CORE_DIRECTORY/data/sql/base/db_world ]; then
+                for f in $CORE_DIRECTORY/data/sql/base/db_world/*.sql; do
+                    if [ ! -z `mysql --defaults-extra-file=$MYSQL_CONFIG --skip-column-names $MYSQL_DATABASE_WORLD -e "SHOW TABLES LIKE '$(basename $f .sql)'"` ]; then
+                        echo -e "\e[0;33mSkipping "$(basename $f)"\e[0m"
+                        continue;
+                    fi
+
+                    echo -e "\e[0;33mImporting "$(basename $f)"\e[0m"
+                    mysql --defaults-extra-file=$MYSQL_CONFIG $MYSQL_DATABASE_WORLD < $f
+                    if [ $? -ne 0 ]; then
+                        rm -rf $MYSQL_CONFIG
+                        exit $?
+                    fi
+                done
+            fi
+        fi
+    fi
+
     rm -rf $MYSQL_CONFIG
 }
 
@@ -29,6 +89,51 @@ function update_database
     echo "port=\"$MYSQL_PORT\"" >> $MYSQL_CONFIG
     echo "user=\"$MYSQL_USERNAME\"" >> $MYSQL_CONFIG
     echo "password=\"$MYSQL_PASSWORD\"" >> $MYSQL_CONFIG
+
+    if [ $1 == 0 ]; then
+        if [ ! -z `mysql --defaults-extra-file=$MYSQL_CONFIG --skip-column-names -e "SHOW DATABASES LIKE '$MYSQL_DATABASE_AUTH'"` ]; then
+            if [ -d $CORE_DIRECTORY/data/sql/updates/db_auth ]; then
+                for f in $CORE_DIRECTORY/data/sql/updates/db_auth/*.sql; do
+                    echo -e "\e[0;33mImporting "$(basename $f)"\e[0m"
+                    mysql --defaults-extra-file=$MYSQL_CONFIG $MYSQL_DATABASE_AUTH < $f
+                    if [ $? -ne 0 ]; then
+                        rm -rf $MYSQL_CONFIG
+                        exit $?
+                    fi
+                done
+            fi
+        fi
+    fi
+
+    if [ $1 == 0 ]; then
+        if [ ! -z `mysql --defaults-extra-file=$MYSQL_CONFIG --skip-column-names -e "SHOW DATABASES LIKE '$MYSQL_DATABASE_CHARACTERS'"` ]; then
+            if [ -d $CORE_DIRECTORY/data/sql/updates/db_characters ]; then
+                for f in $CORE_DIRECTORY/data/sql/updates/db_characters/*.sql; do
+                    echo -e "\e[0;33mImporting "$(basename $f)"\e[0m"
+                    mysql --defaults-extra-file=$MYSQL_CONFIG $MYSQL_DATABASE_CHARACTERS < $f
+                    if [ $? -ne 0 ]; then
+                        rm -rf $MYSQL_CONFIG
+                        exit $?
+                    fi
+                done
+            fi
+        fi
+    fi
+
+    if [ $1 == 0 ]; then
+        if [ ! -z `mysql --defaults-extra-file=$MYSQL_CONFIG --skip-column-names -e "SHOW DATABASES LIKE '$MYSQL_DATABASE_WORLD'"` ]; then
+            if [ -d $CORE_DIRECTORY/data/sql/updates/db_world ]; then
+                for f in $CORE_DIRECTORY/data/sql/updates/db_world/*.sql; do
+                    echo -e "\e[0;33mImporting "$(basename $f)"\e[0m"
+                    mysql --defaults-extra-file=$MYSQL_CONFIG $MYSQL_DATABASE_WORLD < $f
+                    if [ $? -ne 0 ]; then
+                        rm -rf $MYSQL_CONFIG
+                        exit $?
+                    fi
+                done
+            fi
+        fi
+    fi
 
     rm -rf $MYSQL_CONFIG
 }
