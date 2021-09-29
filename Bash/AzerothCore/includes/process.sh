55fdfb00c921c794f05b0f6a6c02b929b336b599
Revision <tkn963@gmail.com>
2021-09-29 12:59:56 +0200

    AC: Processes
    Added a check to the process script to make sure screen is installed when starting or stopping the script. I will, at this time, not be installing screen when calling it since it is installed along with the compilation packages.

--- Bash/AzerothCore/includes/process.sh
+++ Bash/AzerothCore/includes/process.sh
@@ -1,50 +1,54 @@
 #!/bin/bash
 function start_process
 {
-    clear
+    if [ $(dpkg-query -W -f='${Status}' screen 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
+        clear
 
-    printf "${COLOR_GREEN}Starting processes${COLOR_END}\n"
+        printf "${COLOR_GREEN}Starting processes${COLOR_END}\n"
 
-    if [[ -z `screen -list | grep -E "auth"` ]] && [[ -z `screen -list | grep -E "world"` ]]; then
-        if [ -f $CORE_DIRECTORY/bin/start.sh ]; then
-            printf "${COLOR_ORANGE}Starting the server process${COLOR_END}\n"
+        if [[ -z `screen -list | grep -E "auth"` ]] && [[ -z `screen -list | grep -E "world"` ]]; then
+            if [ -f $CORE_DIRECTORY/bin/start.sh ]; then
+                printf "${COLOR_ORANGE}Starting the server process${COLOR_END}\n"
 
-            cd $CORE_DIRECTORY/bin && ./start.sh
+                cd $CORE_DIRECTORY/bin && ./start.sh
+            else
+                printf "${COLOR_ORANGE}Unable to locate the required executable${COLOR_END}\n"
+
+                exit $?
+            fi
         else
-            printf "${COLOR_ORANGE}Unable to locate the required executable${COLOR_END}\n"
-
-            exit $?
+            printf "${COLOR_ORANGE}The server is already running${COLOR_END}\n"
         fi
-    else
-        printf "${COLOR_ORANGE}The server is already running${COLOR_END}\n"
     fi
 }
 
 function stop_process
 {
-    clear
+    if [ $(dpkg-query -W -f='${Status}' screen 2>/dev/null | grep -c "ok installed") -eq 1 ]; then
+        clear
 
-    printf "${COLOR_GREEN}Stopping processes${COLOR_END}\n"
+        printf "${COLOR_GREEN}Stopping processes${COLOR_END}\n"
 
-    if [[ ! -z `screen -list | grep -E "world"` ]]; then
-        printf "${COLOR_ORANGE}Telling the world server to save${COLOR_END}\n"
+        if [[ ! -z `screen -list | grep -E "world"` ]]; then
+            printf "${COLOR_ORANGE}Telling the world server to save${COLOR_END}\n"
 
-        screen -S world -p 0 -X stuff "saveall^m"
+            screen -S world -p 0 -X stuff "saveall^m"
 
-        sleep 3
-    fi
-
-    if [[ ! -z `screen -list | grep -E "auth"` ]] || [[ ! -z `screen -list | grep -E "world"` ]]; then
-        if [ -f $CORE_DIRECTORY/bin/stop.sh ]; then
-            printf "${COLOR_ORANGE}Stopping the server process${COLOR_END}\n"
-
-            cd $CORE_DIRECTORY/bin && ./stop.sh
-        else
-            printf "${COLOR_ORANGE}Unable to locate the required executable${COLOR_END}\n"
-
-            exit $?
+            sleep 3
         fi
-    else
-        printf "${COLOR_ORANGE}No running server found${COLOR_END}\n"
+
+        if [[ ! -z `screen -list | grep -E "auth"` ]] || [[ ! -z `screen -list | grep -E "world"` ]]; then
+            if [ -f $CORE_DIRECTORY/bin/stop.sh ]; then
+                printf "${COLOR_ORANGE}Stopping the server process${COLOR_END}\n"
+
+                cd $CORE_DIRECTORY/bin && ./stop.sh
+            else
+                printf "${COLOR_ORANGE}Unable to locate the required executable${COLOR_END}\n"
+
+                exit $?
+            fi
+        else
+            printf "${COLOR_ORANGE}No running server found${COLOR_END}\n"
+        fi
     fi
 }
