33cdb530988270b746d5e7451849bec5d02fad30
Revision <tkn963@gmail.com>
2021-10-03 18:41:09 +0200

    LUA: Spawn point
    Temporarily disabling the setting of hearthstone. It doesn't work as it should and I don't have time to fix it right now.

--- LUA/AzerothCore/features/spawn_point.lua
+++ LUA/AzerothCore/features/spawn_point.lua
@@ -9,10 +9,10 @@ function spawnOnFirstLogin(event, player)
         if not (player:GetClass() == CLASS_DEATH_KNIGHT) then
             if (player:GetTeam() == TEAM_ALLIANCE) then
                 player:Teleport(SET_SPAWN_POINT[1][5], SET_SPAWN_POINT[1][1], SET_SPAWN_POINT[1][2], SET_SPAWN_POINT[1][3], SET_SPAWN_POINT[1][4])
-                player:SetBindPoint(SET_SPAWN_POINT[1][1], SET_SPAWN_POINT[1][2], SET_SPAWN_POINT[1][3], SET_SPAWN_POINT[1][4], SET_SPAWN_POINT[1][5], SET_SPAWN_POINT[1][6])
+                --player:SetBindPoint(SET_SPAWN_POINT[1][1], SET_SPAWN_POINT[1][2], SET_SPAWN_POINT[1][3], SET_SPAWN_POINT[1][4], SET_SPAWN_POINT[1][5], SET_SPAWN_POINT[1][6])
             elseif (player:GetTeam() == TEAM_HORDE) then
                 player:Teleport(SET_SPAWN_POINT[2][5], SET_SPAWN_POINT[2][1], SET_SPAWN_POINT[2][2], SET_SPAWN_POINT[2][3], SET_SPAWN_POINT[2][4])
-                player:SetBindPoint(SET_SPAWN_POINT[2][1], SET_SPAWN_POINT[2][2], SET_SPAWN_POINT[2][3], SET_SPAWN_POINT[2][4], SET_SPAWN_POINT[2][5], SET_SPAWN_POINT[2][6])
+                --player:SetBindPoint(SET_SPAWN_POINT[2][1], SET_SPAWN_POINT[2][2], SET_SPAWN_POINT[2][3], SET_SPAWN_POINT[2][4], SET_SPAWN_POINT[2][5], SET_SPAWN_POINT[2][6])
             end
         end
     end
