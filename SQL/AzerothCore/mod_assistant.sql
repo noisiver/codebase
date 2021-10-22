76396143252da89ecc007dcdd8c9bd2ae47238a5
Revision <tkn963@gmail.com>
2021-10-22 17:31:18 +0200

    SQL: AzerothCore
    Removed the class spells. I no longer need this thanks to my Assistant module.
    Changed the spawn points for the Assistant to be able to easily change the ids in the future, if required.

--- SQL/AzerothCore/mod_assistant.sql
+++ SQL/AzerothCore/mod_assistant.sql
@@ -1,5 +1,6 @@
+SET @Entry := 9000000;
 -- Spawn points for Stormwind, Orgrimmar and Dalaran
-DELETE FROM `creature` WHERE `id`=9000000;
-INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (9000000, 9000000, 0, -8825.05, 649.331, 94.5545, 4.6664); -- Stormwind City
-INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (9000001, 9000000, 1, 1676.62, -4423.69, 18.9152, 2.64126); -- Orgrimmar
-INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (9000002, 9000000, 571, 5784.29, 623.186, 647.71, 5.75156); -- Dalaran
+DELETE FROM `creature` WHERE `id`=@Entry;
+INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (@Entry, @Entry, 0, -8825.05, 649.331, 94.5545, 4.6664); -- Stormwind City
+INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (@Entry+1, @Entry, 1, 1676.62, -4423.69, 18.9152, 2.64126); -- Orgrimmar
+INSERT INTO `creature` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (@Entry+2, @Entry, 571, 5784.29, 623.186, 647.71, 5.75156); -- Dalaran
