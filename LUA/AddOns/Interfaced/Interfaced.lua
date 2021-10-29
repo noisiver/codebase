36a389231844bdf9bc382da9f29e1e152af3893c
Revision <tkn963@gmail.com>
2021-10-29 23:12:52 +0200

    Interfaced
    Removed the key binding for moving UI elements.
    Added an OnUpdate script that will move the UI elements into position roughly every second IF they are not in the correct position.
    
    I need to rework the handling of the party member frames. We don't want the addon hammering updates into frames that do not need to be updated.

--- LUA/AddOns/Interfaced/Interfaced.lua
+++ LUA/AddOns/Interfaced/Interfaced.lua
@@ -1,33 +1,62 @@
 local INTERFACED, Interfaced = ...
 
 BINDING_HEADER_INTERFACED = "Interfaced"
-BINDING_NAME_SETUIPOINTS = "Set UI Points"
 BINDING_NAME_TOGGLEBARTENDER = "Toggle Bartender Visibility"
 
-function Interfaced_SetUIPoints()
-    PlayerFrame:ClearAllPoints()
-    PlayerFrame:SetPoint("CENTER", -350, 100)
+local playerPositionX = -350
+local playerPositionY = 100
+local targetPositionX = 350
+local targetPositionY = 100
+local petPositionX = 59
+local petPositionY = 52
+local totemPositionX = 45
+local totemPositionY = 48
 
-    TargetFrame:ClearAllPoints()
-    TargetFrame:SetPoint("CENTER", 350, 100)
+local function round(num)
+    return num + (2^52 + 2^51) - (2^52 + 2^51)
+end
 
-    for i = 1, 4 do
-        local frame = _G["PartyMemberFrame"..i]
-        frame:SetScript("OnShow", frame.Hide)
+local function Interfaced_SetUIPoints()
+    local point, relativeTo, relativePoint, xOfs, yOfs = PlayerFrame:GetPoint()
+    if ((not (round(xOfs) == playerPositionX)) or (not (round(yOfs) == playerPositionY))) then
+        PlayerFrame:ClearAllPoints()
+        PlayerFrame:SetPoint("CENTER", playerPositionX, playerPositionY)
     end
 
+    local point, relativeTo, relativePoint, xOfs, yOfs = TargetFrame:GetPoint()
+    if ((not (round(xOfs) == targetPositionX)) or (not (round(yOfs) == targetPositionY))) then
+        TargetFrame:ClearAllPoints()
+        TargetFrame:SetPoint("CENTER", targetPositionX, targetPositionY)
+    end
+
+    --for i = 1, 4 do
+        --local frame = _G["PartyMemberFrame"..i]
+        --frame:SetScript("OnShow", frame.Hide)
+    --end
+
     if (PetFrame) then
-        PetFrame:ClearAllPoints()
-        PetFrame:SetPoint("CENTER", PlayerFrame, "CENTER", 59, 52)
+        local point, relativeTo, relativePoint, xOfs, yOfs = PetFrame:GetPoint()
+        if ((not (round(xOfs) == petPositionX)) or (not (round(yOfs) == petPositionY))) then
+            PetFrame:ClearAllPoints()
+            PetFrame:SetPoint("CENTER", PlayerFrame, "CENTER", petPositionX, petPositionY)
+        end
     end
 
     if (TotemFrame) then
-        TotemFrame:ClearAllPoints()
-        TotemFrame:SetPoint("CENTER", PlayerFrame, "CENTER", 45, 48)
+        local point, relativeTo, relativePoint, xOfs, yOfs = TotemFrame:GetPoint()
+        if ((not (round(xOfs) == totemPositionX)) or (not (round(yOfs) == totemPositionY))) then
+            TotemFrame:ClearAllPoints()
+            TotemFrame:SetPoint("CENTER", PlayerFrame, "CENTER", totemPositionX, totemPositionY)
+        end
     end
 
-    CastingBarFrameBorder:SetTexture(nil)
-    CastingBarFrameFlash:SetTexture(nil)
+    if (CastingBarFrameBorder:GetTexture() ~= nil) then
+        CastingBarFrameBorder:SetTexture(nil)
+    end
+
+    if (CastingBarFrameFlash:GetTexture() ~= nil) then
+        CastingBarFrameFlash:SetTexture(nil)
+    end
 end
 
 function Interfaced_ToggleBartenderVisibility()
@@ -49,9 +78,18 @@ function Interfaced_ToggleBartenderVisibility()
     end
 end
 
-local ef = CreateFrame("FRAME")
-    ef:RegisterEvent("PLAYER_ENTERING_WORLD")
-    ef:SetScript("OnEvent", function(self)
+local uf = CreateFrame("FRAME")
+local time = 0
+uf:SetScript("OnUpdate", function(self, elapsed)
+    time = time + elapsed
+    if (time > 1) then
+        Interfaced_SetUIPoints()
+        time = 0
+    end
+end)
+
+uf:RegisterEvent("PLAYER_ENTERING_WORLD")
+uf:SetScript("OnEvent", function(self)
     Interfaced_SetUIPoints()
     self:UnregisterEvent("PLAYER_ENTERING_WORLD")
     self:SetScript("OnEvent", nil)
