e7ebb9453521ed0076cedd3a31be7fafebc82e11
Revision <tkn963@gmail.com>
2021-10-31 22:31:53 +0100

    Quest loot
    Changed the respawn time of Gnomish Toolbox.

--- SQL/AzerothCore/questloot/gameobject.sql
+++ SQL/AzerothCore/questloot/gameobject.sql
@@ -1,3 +1,7 @@
 -- Cactus Apple (Quest: Galgar's Cactus Apple Surprise (Id: 4402))
 -- Change respawn from 60 to 10 seconds
 UPDATE `gameobject` SET `spawntimesecs`=10 WHERE `id`=171938;
+
+-- Gnomish Toolbox (Quest: From The Wreckage.... (Id: 825))
+-- Change respawn from 900 to 10 seconds
+UPDATE `gameobject` SET `spawntimesecs`=10 WHERE `id`=3236;
