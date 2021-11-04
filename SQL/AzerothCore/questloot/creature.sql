b3e06f44d6beecbaa7be111f6bee55ade8921163
Revision <tkn963@gmail.com>
2021-11-04 15:10:53 +0100

    Quest loot
    I forgot that a creatures respawn time only starts when their body despawns, with that in mind I set the respawn time to 1 of Umpi.

--- SQL/AzerothCore/questloot/creature.sql
+++ SQL/AzerothCore/questloot/creature.sql
@@ -1,3 +1,3 @@
 -- Umpi (Quest: Elixir of Suffering (Id: 499))
--- Change respawn time from 300 to 3 seconds
-UPDATE `creature` SET `spawntimesecs`=3 WHERE `id`=2230;
+-- Change respawn time from 300 to 1 seconds
+UPDATE `creature` SET `spawntimesecs`=1 WHERE `id`=2230;
