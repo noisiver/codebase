6e7ccf5d14aca08aa66dd6ccd1d08d8f0a9062c5
Revision <tkn963@gmail.com>
2021-12-15 03:53:31 +0100

    Spell reagents
    Added a script that makes all reagents that are required by spells have no cost.

---
+++ SQL/AzerothCore/free_reagents.sql
@@ -0,0 +1,44 @@
+-- Infernal Stone
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=5565;
+-- Arcane Powder
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17020;
+-- Wild Berries
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17021;
+-- Wild Thornroot
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17026;
+-- Holy Candle
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17028;
+-- Sacred Candle
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17029;
+-- Ankh
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17030;
+-- Rune of Teleportation
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17031;
+-- Rune of Portals
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17032;
+-- Symbol of Divinity
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17033;
+-- Maple Seed
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17034;
+-- Stranglethorn Seed
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17035;
+-- Ashwood Seed
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17036;
+-- Hornbeam Seed
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17037;
+-- Ironwood Seed
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=17038;
+-- Symbol of Kings
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=21177;
+-- Flintweed Seed
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=22147;
+-- Wild Quillvine
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=22148;
+-- Corpse Dust
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=37201;
+-- Wild Spineleaf
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=44605;
+-- Starleaf Seed
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=44614;
+-- Devout Candle
+UPDATE `item_template` SET `BuyPrice`=0, `SellPrice`=0 WHERE `entry`=44615;
