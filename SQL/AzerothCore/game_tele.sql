e6705af3fe993f1a41807ba1a0a3a4c615c5fc4d
Revision <tkn963@gmail.com>
2021-12-28 14:51:34 +0100

    Module
    Changed the name of my Refer A Friend module back to Recruit A Friend like it was before.

---
+++ SQL/AzerothCore/game_tele.sql
@@ -0,0 +1,4 @@
+DELETE FROM `game_tele` WHERE `name` = 'SouthSeas';
+INSERT INTO `game_tele` (`position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (-11380.3, -4751.17, 6.12476, 1.08306, 1, 'SouthSeas');
+DELETE FROM `game_tele` WHERE `name` = 'SilithusFarm';
+INSERT INTO `game_tele` (`id`, `position_x`, `position_y`, `position_z`, `orientation`, `map`, `name`) VALUES (-10735.7, 2495.88, 6.4513, 4.85769, 1, 'SilithusFarm');
