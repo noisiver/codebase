b5bb9d529370d7fa32c17c97ee97fbb537c8e79f
Revision <tkn963@gmail.com>
2022-01-15 02:46:28 +0100

    Fix
    Updated to support the new table layout.

--- SQL/AzerothCore/mod_assistant.sql
+++ SQL/AzerothCore/mod_assistant.sql
@@ -1,6 +1,6 @@
 SET @Entry := 9000000;
 -- Spawn points for Stormwind, Orgrimmar and Dalaran
-DELETE FROM `creature` WHERE `creature_id1`=@Entry;
-INSERT INTO `creature` (`guid`, `creature_id1`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (@Entry, @Entry, 0, -8825.05, 649.331, 94.5545, 4.6664); -- Stormwind City
-INSERT INTO `creature` (`guid`, `creature_id1`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (@Entry+1, @Entry, 1, 1676.62, -4423.69, 18.9152, 2.64126); -- Orgrimmar
-INSERT INTO `creature` (`guid`, `creature_id1`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (@Entry+2, @Entry, 571, 5784.29, 623.186, 647.71, 5.75156); -- Dalaran
+DELETE FROM `creature` WHERE `id1`=@Entry;
+INSERT INTO `creature` (`guid`, `id1`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (@Entry, @Entry, 0, -8825.05, 649.331, 94.5545, 4.6664); -- Stormwind City
+INSERT INTO `creature` (`guid`, `id1`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (@Entry+1, @Entry, 1, 1676.62, -4423.69, 18.9152, 2.64126); -- Orgrimmar
+INSERT INTO `creature` (`guid`, `id1`, `map`, `position_x`, `position_y`, `position_z`, `orientation`) VALUES (@Entry+2, @Entry, 571, 5784.29, 623.186, 647.71, 5.75156); -- Dalaran
