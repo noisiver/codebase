3c8c6c70ba7ff539025046b119d16bf8bd64e37d
Revision <tkn963@gmail.com>
2022-05-21 20:14:10 +0200

    Hack
    Added a temporary hack that fixes an issue with the changed data files. I'll remove it when a new version of the files are released and, hopefully, the tags are fixed.

--- Bash/AzerothCore/azerothcore.sh
+++ Bash/AzerothCore/azerothcore.sh
@@ -2374,6 +2374,11 @@ function get_client_files
         # Grab the latest version available on github
         AVAILABLE_VERSION=$(git ls-remote --tags --sort="v:refname" https://github.com/wowgaming/client-data.git | tail -n1 | cut --delimiter='/' --fields=3 | sed 's/v//')
 
+        # Temporary hack due to changes
+        if [[ $AVAILABLE_VERSION == 15 ]]; then
+            AVAILABLE_VERSION=14
+        fi
+
         # Check if the latest version differ from the required version
         if [[ $OPTION_SOURCE_REQUIRED_CLIENT_DATA != $AVAILABLE_VERSION ]]; then
             # Update the required version with this version
