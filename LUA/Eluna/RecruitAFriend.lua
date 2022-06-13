737b03178cbb2975688a6cc0f35de86c1aafc650
Revision <tkn963@gmail.com>
2022-06-14 00:49:29 +0200

    Correction
    I accidentally set a default value where there shouldn't be one.

--- LUA/Eluna/RecruitAFriend.lua
+++ LUA/Eluna/RecruitAFriend.lua
@@ -19,7 +19,7 @@ local Event   = {
     OnCommand = 42,
 }
 
-AuthDBQuery('CREATE TABLE IF NOT EXISTS `'..Config.Database..'`.`recruit_a_friend_accounts` (`account_id` INT(10) UNSIGNED NOT NULL, `recruiter_id` INT(10) UNSIGNED NOT NULL, `referral_date` TIMESTAMP NOT NULL DEFAULT current_timestamp(), `status` TINYINT(3) UNSIGNED NOT NULL DEFAULT \'1\', PRIMARY KEY (`account_id`) USING BTREE) COLLATE=\'utf8mb4_general_ci\' ENGINE=InnoDB;')
+AuthDBQuery('CREATE TABLE IF NOT EXISTS `'..Config.Database..'`.`recruit_a_friend_accounts` (`account_id` INT(10) UNSIGNED NOT NULL, `recruiter_id` INT(10) UNSIGNED NOT NULL, `referral_date` TIMESTAMP NOT NULL DEFAULT current_timestamp(), `status` TINYINT(3) UNSIGNED NOT NULL , PRIMARY KEY (`account_id`) USING BTREE) COLLATE=\'utf8mb4_general_ci\' ENGINE=InnoDB;')
 AuthDBQuery('CREATE TABLE IF NOT EXISTS `'..Config.Database..'`.`recruit_a_friend_rewarded` (`account_id` INT(10) UNSIGNED NOT NULL, `realm_id` INT(10) UNSIGNED NOT NULL, `character_guid` INT(10) UNSIGNED NOT NULL, PRIMARY KEY (`account_id`, `realm_id`, `character_guid`) USING BTREE) COLLATE=\'utf8mb4_general_ci\' ENGINE=InnoDB;')
 AuthDBQuery('DELETE FROM `'..Config.Database..'`.`recruit_a_friend_accounts` WHERE `status` = '..Status.Pending..';')
 
