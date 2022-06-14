33a0c802cb082cc667233889da5ce9432329ff72
Revision <tkn963@gmail.com>
2022-06-14 15:40:53 +0200

    Spawn Points
    Since I removed the spawn points module, this will take its place. It's less code but the same results.

---
+++ SQL/AzerothCore/world/playercreateinfo.sql
@@ -0,0 +1,2 @@
+-- Set all spawn points to Dalaran. Death Knight not included
+UPDATE `playercreateinfo` SET `map`=571, `position_x`=5807.779, `position_y`=588.14, `position_z`=660.93, `orientation`=1.65 WHERE `class` NOT LIKE 6;
