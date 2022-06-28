fa42d9223b75f213424193b67b7f6dc8fd1ba0a7
Revision <tkn963@gmail.com>
2022-06-28 17:49:15 +0200

    Removed
    Removed all files I no longer use and therefore no longer support.

--- SQL/AzerothCore/auth/realmlist.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `realmlist` WHERE `id`=1;
-INSERT INTO `realmlist` (`id`, `name`, `address`, `localAddress`, `localSubnetMask`, `port`, `gamebuild`) VALUES (1, 'AzerothCore', '127.0.0.1', '127.0.0.1', '255.255.255.0', 8085, 12340);
--- SQL/AzerothCore/world/playercreateinfo.sql
+++
@@ -1,2 +0,0 @@
--- Set all spawn points to Dalaran. Death Knight not included
-UPDATE `playercreateinfo` SET `map`=571, `position_x`=5807.779, `position_y`=588.14, `position_z`=660.93, `orientation`=1.65 WHERE `class` NOT LIKE 6;
