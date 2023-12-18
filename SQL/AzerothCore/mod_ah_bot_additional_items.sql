5b8599bdd96dd6120c81e590e396e8382101dba2
Revision <tkn963@gmail.com>
2023-12-18 22:30:32 +0100

    AH Bot
    Added changes I've done to the AH Bot module. I added another table to that's used to make the bot post items listed in it. The price of the items will probably need to be tweaked.

---
+++ SQL/AzerothCore/mod_ah_bot_additional_items.sql
@@ -0,0 +1,451 @@
+UPDATE `item_template` SET `BuyPrice`=400, `SellPrice`=400 WHERE `entry` IN (40896, 40897, 40899, 40900, 40901, 40902, 40903, 40906, 40908, 40909, 40912, 40913, 40914, 40915, 40916, 40919, 40920, 40921, 40922, 40923, 40924, 41092, 41094, 41095, 41096, 41097, 41098, 41099, 41100, 41101, 41102, 41103, 41104, 41105, 41106, 41107, 41108, 41109, 41110, 41517, 41518, 41524, 41526, 41527, 41529, 41530, 41531, 41532, 41533, 41534, 41535, 41536, 41537, 41538, 41539, 41540, 41541, 41542, 41547, 41552, 42396, 42397, 42398, 42399, 42400, 42401, 42402, 42403, 42404, 42405, 42406, 42407, 42408, 42409, 42410, 42411, 42412, 42414, 42415, 42416, 42417, 42453, 42454, 42455, 42456, 42457, 42458, 42459, 42460, 42461, 42462, 42463, 42464, 42465, 42466, 42467, 42468, 42469, 42470, 42471, 42473, 42734, 42735, 42736, 42737, 42738, 42739, 42740, 42741, 42742, 42743, 42744, 42745, 42746, 42747, 42748, 42749, 42750, 42751, 42752, 42753, 42754, 42897, 42898, 42899, 42900, 42901, 42902, 42903, 42904, 42905, 42906, 42907, 42908, 42909, 42910, 42911, 42912, 42913, 42914, 42915, 42916, 42917, 42954, 42955, 42956, 42957, 42958, 42959, 42960, 42961, 42962, 42963, 42964, 42965, 42966, 42967, 42968, 42969, 42970, 42971, 42972, 42973, 42974, 43316, 43331, 43332, 43334, 43335, 43338, 43339, 43340, 43342, 43343, 43344, 43350, 43351, 43354, 43355, 43356, 43357, 43359, 43360, 43361, 43362, 43364, 43365, 43366, 43367, 43368, 43369, 43370, 43371, 43372, 43373, 43374, 43376, 43377, 43378, 43379, 43380, 43381, 43385, 43386, 43388, 43389, 43390, 43391, 43392, 43393, 43394, 43395, 43396, 43397, 43398, 43399, 43400, 43412, 43413, 43414, 43415, 43416, 43417, 43418, 43419, 43420, 43421, 43422, 43423, 43424, 43425, 43426, 43427, 43428, 43429, 43430, 43431, 43432, 43533, 43534, 43535, 43536, 43537, 43538, 43539, 43541, 43542, 43543, 43544, 43545, 43546, 43547, 43548, 43549, 43550, 43551, 43552, 43553, 43554, 43671, 43672, 43673, 43674, 43725, 43825, 43826, 43827, 43867, 43868, 43869, 44684, 44920, 44922, 44923, 44928, 44955, 45601, 45602, 45603, 45604, 45622, 45623, 45625, 45731, 45732, 45733, 45734, 45735, 45736, 45737, 45738, 45739, 45740, 45741, 45742, 45743, 45744, 45745, 45746, 45747, 45753, 45755, 45756, 45757, 45758, 45760, 45761, 45762, 45764, 45766, 45767, 45768, 45769, 45770, 45771, 45772, 45775, 45776, 45777, 45778, 45779, 45780, 45781, 45782, 45783, 45785, 45789, 45790, 45792, 45793, 45794, 45795, 45797, 45799, 45800, 45803, 45804, 45805, 45806, 46372, 48720, 49084, 50045, 50077, 50125);
+UPDATE `item_template` SET `BuyPrice`=360000, `SellPrice`=90000 WHERE `entry` IN (40111, 40112, 40113, 40114, 40115, 40116, 40117, 40118, 40119, 40120, 40121, 40122, 40123, 40124, 40125, 40126, 40127, 40128, 40129, 40130, 40131, 40132, 40133, 40134, 40135, 40136, 40137, 40138, 40139, 40140, 40141, 40142, 40143, 40144, 40145, 40146, 40147, 40148, 40149, 40150, 40151, 40152, 40153, 40154, 40155, 40156, 40157, 40158, 40159, 40160, 40161, 40162, 40163, 40164, 40165, 40166, 40167, 40168, 40169, 40170, 40171, 40172, 40173, 40174, 40175, 40176, 40177, 40178, 40179, 40180, 40181, 40182);
+UPDATE `item_template` SET `BuyPrice`=240000, `SellPrice`=60000 WHERE `entry` IN (41285, 41307, 41333, 41335, 41339, 41375, 41376, 41377, 41378, 41379, 41380, 41381, 41382, 41385, 41389, 41395, 41396, 41397, 41398, 41400, 41401);
+UPDATE `item_template` SET `BuyPrice`=45000, `SellPrice`=11250 WHERE `entry` IN (45862, 45879, 45880, 45881, 45882, 45883, 45987);
+DELETE FROM `mod_auctionhousebot_additional_items`;
+INSERT INTO `mod_auctionhousebot_additional_items` (`item`) VALUES
+(40111), -- Bold Cardinal Ruby
+(40112), -- Delicate Cardinal Ruby
+(40113), -- Runed Cardinal Ruby
+(40114), -- Bright Cardinal Ruby
+(40115), -- Subtle Cardinal Ruby
+(40116), -- Flashing Cardinal Ruby
+(40117), -- Fractured Cardinal Ruby
+(40118), -- Precise Cardinal Ruby
+(40119), -- Solid Majestic Zircon
+(40120), -- Sparkling Majestic Zircon
+(40121), -- Lustrous Majestic Zircon
+(40122), -- Stormy Majestic Zircon
+(40123), -- Brilliant King's Amber
+(40124), -- Smooth King's Amber
+(40125), -- Rigid King's Amber
+(40126), -- Thick King's Amber
+(40127), -- Mystic King's Amber
+(40128), -- Quick King's Amber
+(40129), -- Sovereign Dreadstone
+(40130), -- Shifting Dreadstone
+(40131), -- Tenuous Dreadstone
+(40132), -- Glowing Dreadstone
+(40133), -- Purified Dreadstone
+(40134), -- Royal Dreadstone
+(40135), -- Mysterious Dreadstone
+(40136), -- Balanced Dreadstone
+(40137), -- Infused Dreadstone
+(40138), -- Regal Dreadstone
+(40139), -- Defender's Dreadstone
+(40140), -- Puissant Dreadstone
+(40141), -- Guardian's Dreadstone
+(40142), -- Inscribed Ametrine
+(40143), -- Etched Ametrine
+(40144), -- Champion's Ametrine
+(40145), -- Resplendent Ametrine
+(40146), -- Fierce Ametrine
+(40147), -- Deadly Ametrine
+(40148), -- Glinting Ametrine
+(40149), -- Lucent Ametrine
+(40150), -- Deft Ametrine
+(40151), -- Luminous Ametrine
+(40152), -- Potent Ametrine
+(40153), -- Veiled Ametrine
+(40154), -- Durable Ametrine
+(40155), -- Reckless Ametrine
+(40156), -- Wicked Ametrine
+(40157), -- Pristine Ametrine
+(40158), -- Empowered Ametrine
+(40159), -- Stark Ametrine
+(40160), -- Stalwart Ametrine
+(40161), -- Glimmering Ametrine
+(40162), -- Accurate Ametrine
+(40163), -- Resolute Ametrine
+(40164), -- Timeless Eye of Zul
+(40165), -- Jagged Eye of Zul
+(40166), -- Vivid Eye of Zul
+(40167), -- Enduring Eye of Zul
+(40168), -- Steady Eye of Zul
+(40169), -- Forceful Eye of Zul
+(40170), -- Seer's Eye of Zul
+(40171), -- Misty Eye of Zul
+(40172), -- Shining Eye of Zul
+(40173), -- Turbid Eye of Zul
+(40174), -- Intricate Eye of Zul
+(40175), -- Dazzling Eye of Zul
+(40176), -- Sundered Eye of Zul
+(40177), -- Lambent Eye of Zul
+(40178), -- Opaque Eye of Zul
+(40179), -- Energized Eye of Zul
+(40180), -- Radiant Eye of Zul
+(40181), -- Tense Eye of Zul
+(40182), -- Shattered Eye of Zul
+(40896), -- Glyph of Frenzied Regeneration
+(40897), -- Glyph of Maul
+(40899), -- Glyph of Growl
+(40900), -- Glyph of Mangle
+(40901), -- Glyph of Shred
+(40902), -- Glyph of Rip
+(40903), -- Glyph of Rake
+(40906), -- Glyph of Swiftmend
+(40908), -- Glyph of Innervate
+(40909), -- Glyph of Rebirth
+(40912), -- Glyph of Regrowth
+(40913), -- Glyph of Rejuvenation
+(40914), -- Glyph of Healing Touch
+(40915), -- Glyph of Lifebloom
+(40916), -- Glyph of Starfire
+(40919), -- Glyph of Insect Swarm
+(40920), -- Glyph of Hurricane
+(40921), -- Glyph of Starfall
+(40922), -- Glyph of Wrath
+(40923), -- Glyph of Moonfire
+(40924), -- Glyph of Entangling Roots
+(41092), -- Glyph of Judgement
+(41094), -- Glyph of Seal of Command
+(41095), -- Glyph of Hammer of Justice
+(41096), -- Glyph of Spiritual Attunement
+(41097), -- Glyph of Hammer of Wrath
+(41098), -- Glyph of Crusader Strike
+(41099), -- Glyph of Consecration
+(41100), -- Glyph of Righteous Defense
+(41101), -- Glyph of Avenger's Shield
+(41102), -- Glyph of Turn Evil
+(41103), -- Glyph of Exorcism
+(41104), -- Glyph of Cleansing
+(41105), -- Glyph of Flash of Light
+(41106), -- Glyph of Holy Light
+(41107), -- Glyph of Avenging Wrath
+(41108), -- Glyph of Divinity
+(41109), -- Glyph of Seal of Wisdom
+(41110), -- Glyph of Seal of Light
+(41285), -- Chaotic Skyflare Diamond
+(41307), -- Destructive Skyflare Diamond
+(41333), -- Ember Skyflare Diamond
+(41335), -- Enigmatic Skyflare Diamond
+(41339), -- Swift Skyflare Diamond
+(41375), -- Tireless Skyflare Diamond
+(41376), -- Revitalizing Skyflare Diamond
+(41377), -- Effulgent Skyflare Diamond
+(41378), -- Forlorn Skyflare Diamond
+(41379), -- Impassive Skyflare Diamond
+(41380), -- Austere Earthsiege Diamond
+(41381), -- Persistent Earthsiege Diamond
+(41382), -- Trenchant Earthsiege Diamond
+(41385), -- Invigorating Earthsiege Diamond
+(41389), -- Beaming Earthsiege Diamond
+(41395), -- Bracing Earthsiege Diamond
+(41396), -- Eternal Earthsiege Diamond
+(41397), -- Powerful Earthsiege Diamond
+(41398), -- Relentless Earthsiege Diamond
+(41400), -- Thundering Skyflare Diamond
+(41401), -- Insightful Earthsiege Diamond
+(41517), -- Glyph of Chain Heal
+(41518), -- Glyph of Chain Lightning
+(41524), -- Glyph of Lava
+(41526), -- Glyph of Shocking
+(41527), -- Glyph of Earthliving Weapon
+(41529), -- Glyph of Fire Elemental Totem
+(41530), -- Glyph of Fire Nova
+(41531), -- Glyph of Flame Shock
+(41532), -- Glyph of Flametongue Weapon
+(41533), -- Glyph of Healing Stream Totem
+(41534), -- Glyph of Healing Wave
+(41535), -- Glyph of Lesser Healing Wave
+(41536), -- Glyph of Lightning Bolt
+(41537), -- Glyph of Lightning Shield
+(41538), -- Glyph of Mana Tide Totem
+(41539), -- Glyph of Stormstrike
+(41540), -- Glyph of Lava Lash
+(41541), -- Glyph of Water Mastery
+(41542), -- Glyph of Windfury Weapon
+(41547), -- Glyph of Frost Shock
+(41552), -- Glyph of Elemental Mastery
+(42396), -- Glyph of Circle of Healing
+(42397), -- Glyph of Dispel Magic
+(42398), -- Glyph of Fade
+(42399), -- Glyph of Fear Ward
+(42400), -- Glyph of Flash Heal
+(42401), -- Glyph of Holy Nova
+(42402), -- Glyph of Inner Fire
+(42403), -- Glyph of Lightwell
+(42404), -- Glyph of Mass Dispel
+(42405), -- Glyph of Mind Control
+(42406), -- Glyph of Shadow Word: Pain
+(42407), -- Glyph of Shadow
+(42408), -- Glyph of Power Word: Shield
+(42409), -- Glyph of Prayer of Healing
+(42410), -- Glyph of Psychic Scream
+(42411), -- Glyph of Renew
+(42412), -- Glyph of Scourge Imprisonment
+(42414), -- Glyph of Shadow Word: Death
+(42415), -- Glyph of Mind Flay
+(42416), -- Glyph of Smite
+(42417), -- Glyph of Spirit of Redemption
+(42453), -- Glyph of Incinerate
+(42454), -- Glyph of Conflagrate
+(42455), -- Glyph of Corruption
+(42456), -- Glyph of Curse of Agony
+(42457), -- Glyph of Death Coil
+(42458), -- Glyph of Fear
+(42459), -- Glyph of Felguard
+(42460), -- Glyph of Felhunter
+(42461), -- Glyph of Health Funnel
+(42462), -- Glyph of Healthstone
+(42463), -- Glyph of Howl of Terror
+(42464), -- Glyph of Immolate
+(42465), -- Glyph of Imp
+(42466), -- Glyph of Searing Pain
+(42467), -- Glyph of Shadow Bolt
+(42468), -- Glyph of Shadowburn
+(42469), -- Glyph of Siphon Life
+(42470), -- Glyph of Soulstone
+(42471), -- Glyph of Succubus
+(42473), -- Glyph of Voidwalker
+(42734), -- Glyph of Arcane Explosion
+(42735), -- Glyph of Arcane Missiles
+(42736), -- Glyph of Arcane Power
+(42737), -- Glyph of Blink
+(42738), -- Glyph of Evocation
+(42739), -- Glyph of Fireball
+(42740), -- Glyph of Fire Blast
+(42741), -- Glyph of Frost Nova
+(42742), -- Glyph of Frostbolt
+(42743), -- Glyph of Ice Armor
+(42744), -- Glyph of Ice Block
+(42745), -- Glyph of Ice Lance
+(42746), -- Glyph of Icy Veins
+(42747), -- Glyph of Scorch
+(42748), -- Glyph of Invisibility
+(42749), -- Glyph of Mage Armor
+(42750), -- Glyph of Mana Gem
+(42751), -- Glyph of Molten Armor
+(42752), -- Glyph of Polymorph
+(42753), -- Glyph of Remove Curse
+(42754), -- Glyph of Water Elemental
+(42897), -- Glyph of Aimed Shot
+(42898), -- Glyph of Arcane Shot
+(42899), -- Glyph of the Beast
+(42900), -- Glyph of Mending
+(42901), -- Glyph of Aspect of the Viper
+(42902), -- Glyph of Bestial Wrath
+(42903), -- Glyph of Deterrence
+(42904), -- Glyph of Disengage
+(42905), -- Glyph of Freezing Trap
+(42906), -- Glyph of Frost Trap
+(42907), -- Glyph of Hunter's Mark
+(42908), -- Glyph of Immolation Trap
+(42909), -- Glyph of the Hawk
+(42910), -- Glyph of Multi-Shot
+(42911), -- Glyph of Rapid Fire
+(42912), -- Glyph of Serpent Sting
+(42913), -- Glyph of Snake Trap
+(42914), -- Glyph of Steady Shot
+(42915), -- Glyph of Trueshot Aura
+(42916), -- Glyph of Volley
+(42917), -- Glyph of Wyvern Sting
+(42954), -- Glyph of Adrenaline Rush
+(42955), -- Glyph of Ambush
+(42956), -- Glyph of Backstab
+(42957), -- Glyph of Blade Flurry
+(42958), -- Glyph of Crippling Poison
+(42959), -- Glyph of Deadly Throw
+(42960), -- Glyph of Evasion
+(42961), -- Glyph of Eviscerate
+(42962), -- Glyph of Expose Armor
+(42963), -- Glyph of Feint
+(42964), -- Glyph of Garrote
+(42965), -- Glyph of Ghostly Strike
+(42966), -- Glyph of Gouge
+(42967), -- Glyph of Hemorrhage
+(42968), -- Glyph of Preparation
+(42969), -- Glyph of Rupture
+(42970), -- Glyph of Sap
+(42971), -- Glyph of Vigor
+(42972), -- Glyph of Sinister Strike
+(42973), -- Glyph of Slice and Dice
+(42974), -- Glyph of Sprint
+(43316), -- Glyph of Aquatic Form
+(43331), -- Glyph of Unburdened Rebirth
+(43332), -- Glyph of Thorns
+(43334), -- Glyph of Challenging Roar
+(43335), -- Glyph of the Wild
+(43338), -- Glyph of Revive Pet
+(43339), -- Glyph of Arcane Intellect
+(43340), -- Glyph of Blessing of Might
+(43342), -- Glyph of Fading
+(43343), -- Glyph of Pick Pocket
+(43344), -- Glyph of Water Breathing
+(43350), -- Glyph of Mend Pet
+(43351), -- Glyph of Feign Death
+(43354), -- Glyph of Possessed Strength
+(43355), -- Glyph of the Pack
+(43356), -- Glyph of Scare Beast
+(43357), -- Glyph of Fire Ward
+(43359), -- Glyph of Frost Armor
+(43360), -- Glyph of Frost Ward
+(43361), -- Glyph of the Penguin
+(43362), -- Glyph of the Bear Cub
+(43364), -- Glyph of Slow Fall
+(43365), -- Glyph of Blessing of Kings
+(43366), -- Glyph of Blessing of Wisdom
+(43367), -- Glyph of Lay on Hands
+(43368), -- Glyph of Sense Undead
+(43369), -- Glyph of the Wise
+(43370), -- Glyph of Levitate
+(43371), -- Glyph of Fortitude
+(43372), -- Glyph of Shadow Protection
+(43373), -- Glyph of Shackle Undead
+(43374), -- Glyph of Shadowfiend
+(43376), -- Glyph of Distract
+(43377), -- Glyph of Pick Lock
+(43378), -- Glyph of Safe Fall
+(43379), -- Glyph of Blurred Speed
+(43380), -- Glyph of Vanish
+(43381), -- Glyph of Astral Recall
+(43385), -- Glyph of Renewed Life
+(43386), -- Glyph of Water Shield
+(43388), -- Glyph of Water Walking
+(43389), -- Glyph of Unending Breath
+(43390), -- Glyph of Drain Soul
+(43391), -- Glyph of Kilrogg
+(43392), -- Glyph of Curse of Exhaustion
+(43393), -- Glyph of Enslave Demon
+(43394), -- Glyph of Souls
+(43395), -- Glyph of Battle
+(43396), -- Glyph of Bloodrage
+(43397), -- Glyph of Charge
+(43398), -- Glyph of Mocking Blow
+(43399), -- Glyph of Thunder Clap
+(43400), -- Glyph of Enduring Victory
+(43412), -- Glyph of Bloodthirst
+(43413), -- Glyph of Rapid Charge
+(43414), -- Glyph of Cleaving
+(43415), -- Glyph of Devastate
+(43416), -- Glyph of Execution
+(43417), -- Glyph of Hamstring
+(43418), -- Glyph of Heroic Strike
+(43419), -- Glyph of Intervene
+(43420), -- Glyph of Barbaric Insults
+(43421), -- Glyph of Mortal Strike
+(43422), -- Glyph of Overpower
+(43423), -- Glyph of Rending
+(43424), -- Glyph of Revenge
+(43425), -- Glyph of Blocking
+(43426), -- Glyph of Last Stand
+(43427), -- Glyph of Sunder Armor
+(43428), -- Glyph of Sweeping Strikes
+(43429), -- Glyph of Taunt
+(43430), -- Glyph of Resonating Power
+(43431), -- Glyph of Victory Rush
+(43432), -- Glyph of Whirlwind
+(43533), -- Glyph of Anti-Magic Shell
+(43534), -- Glyph of Heart Strike
+(43535), -- Glyph of Blood Tap
+(43536), -- Glyph of Bone Shield
+(43537), -- Glyph of Chains of Ice
+(43538), -- Glyph of Dark Command
+(43539), -- Glyph of Death's Embrace
+(43541), -- Glyph of Death Grip
+(43542), -- Glyph of Death and Decay
+(43543), -- Glyph of Frost Strike
+(43544), -- Glyph of Horn of Winter
+(43545), -- Glyph of Icebound Fortitude
+(43546), -- Glyph of Icy Touch
+(43547), -- Glyph of Obliterate
+(43548), -- Glyph of Plague Strike
+(43549), -- Glyph of the Ghoul
+(43550), -- Glyph of Rune Strike
+(43551), -- Glyph of Scourge Strike
+(43552), -- Glyph of Strangulate
+(43553), -- Glyph of Unbreakable Armor
+(43554), -- Glyph of Vampiric Blood
+(43671), -- Glyph of Corpse Explosion
+(43672), -- Glyph of Pestilence
+(43673), -- Glyph of Raise Dead
+(43674), -- Glyph of Dash
+(43725), -- Glyph of Ghost Wolf
+(43825), -- Glyph of Rune Tap
+(43826), -- Glyph of Blood Strike
+(43827), -- Glyph of Death Strike
+(43867), -- Glyph of Holy Wrath
+(43868), -- Glyph of Seal of Righteousness
+(43869), -- Glyph of Seal of Vengeance
+(44684), -- Glyph of Frostfire
+(44920), -- Glyph of Blast Wave
+(44922), -- Glyph of Typhoon
+(44923), -- Glyph of Thunderstorm
+(44928), -- Glyph of Focus
+(44955), -- Glyph of Arcane Blast
+(45601), -- Glyph of Berserk
+(45602), -- Glyph of Wild Growth
+(45603), -- Glyph of Nourish
+(45604), -- Glyph of Savage Roar
+(45622), -- Glyph of Monsoon
+(45623), -- Glyph of Barkskin
+(45625), -- Glyph of Chimera Shot
+(45731), -- Glyph of Explosive Shot
+(45732), -- Glyph of Kill Shot
+(45733), -- Glyph of Explosive Trap
+(45734), -- Glyph of Scatter Shot
+(45735), -- Glyph of Raptor Strike
+(45736), -- Glyph of Deep Freeze
+(45737), -- Glyph of Living Bomb
+(45738), -- Glyph of Arcane Barrage
+(45739), -- Glyph of Mirror Image
+(45740), -- Glyph of Ice Barrier
+(45741), -- Glyph of Beacon of Light
+(45742), -- Glyph of Hammer of the Righteous
+(45743), -- Glyph of Divine Storm
+(45744), -- Glyph of Shield of Righteousness
+(45745), -- Glyph of Divine Plea
+(45746), -- Glyph of Holy Shock
+(45747), -- Glyph of Salvation
+(45753), -- Glyph of Dispersion
+(45755), -- Glyph of Guardian Spirit
+(45756), -- Glyph of Penance
+(45757), -- Glyph of Mind Sear
+(45758), -- Glyph of Hymn of Hope
+(45760), -- Glyph of Pain Suppression
+(45761), -- Glyph of Hunger for Blood
+(45762), -- Glyph of Killing Spree
+(45764), -- Glyph of Shadow Dance
+(45766), -- Glyph of Fan of Knives
+(45767), -- Glyph of Tricks of the Trade
+(45768), -- Glyph of Mutilate
+(45769), -- Glyph of Cloak of Shadows
+(45770), -- Glyph of Thunder
+(45771), -- Glyph of Feral Spirit
+(45772), -- Glyph of Riptide
+(45775), -- Glyph of Earth Shield
+(45776), -- Glyph of Totem of Wrath
+(45777), -- Glyph of Hex
+(45778), -- Glyph of Stoneclaw Totem
+(45779), -- Glyph of Haunt
+(45780), -- Glyph of Metamorphosis
+(45781), -- Glyph of Chaos Bolt
+(45782), -- Glyph of Demonic Circle
+(45783), -- Glyph of Shadowflame
+(45785), -- Glyph of Life Tap
+(45789), -- Glyph of Soul Link
+(45790), -- Glyph of Bladestorm
+(45792), -- Glyph of Shockwave
+(45793), -- Glyph of Vigilance
+(45794), -- Glyph of Enraged Regeneration
+(45795), -- Glyph of Spell Reflection
+(45797), -- Glyph of Shield Wall
+(45799), -- Glyph of Dancing Rune Weapon
+(45800), -- Glyph of Hungering Cold
+(45803), -- Glyph of Unholy Blight
+(45804), -- Glyph of Dark Death
+(45805), -- Glyph of Disease
+(45806), -- Glyph of Howling Blast
+(45862), -- Bold Stormjewel
+(45879), -- Delicate Stormjewel
+(45880), -- Solid Stormjewel
+(45881), -- Sparkling Stormjewel
+(45882), -- Brilliant Stormjewel
+(45883), -- Runed Stormjewel
+(45987), -- Rigid Stormjewel
+(46372), -- Glyph of Survival Instincts
+(48720), -- Glyph of Claw
+(49084), -- Glyph of Command
+(50045), -- Glyph of Eternal Water
+(50077), -- Glyph of Quick Decay
+(50125); -- Glyph of Rapid Rejuvenation
