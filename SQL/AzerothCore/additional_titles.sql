89ddb51a1d1310f035638f1ecc742b3e23f3e322
Revision <tkn963@gmail.com>
2025-07-07 20:41:37 +0200

    Added a query to get titles for reaching level 80 as a class, as a race or for reaching 450 in a profession

---
+++ SQL/AzerothCore/additional_titles.sql
@@ -0,0 +1,39 @@
+DELETE FROM `achievement_reward` WHERE `ID` IN (458, 459, 460, 461, 462, 463, 464, 465, 466, 467, 1404, 1405, 1406, 1407, 1408, 1409, 1410, 1411, 1412, 1413, 1414, 1415, 1416, 1417, 1418, 1419, 1420, 1421, 1422, 1423, 1424, 1425, 1426, 1427);
+INSERT INTO `achievement_reward` (`ID`, `TitleA`, `TitleH`, `ItemID`, `Sender`, `Subject`, `Body`, `MailTemplateID`) VALUES
+-- Classes
+(458, 95, 95, 0, 0, NULL, NULL, 0), -- Rogue
+(459, 94, 94, 0, 0, NULL, NULL, 0), -- Warrior
+(460, 93, 93, 0, 0, NULL, NULL, 0), -- Mage
+(461, 92, 92, 0, 0, NULL, NULL, 0), -- Death Knight
+(462, 91, 91, 0, 0, NULL, NULL, 0), -- Hunter
+(463, 90, 90, 0, 0, NULL, NULL, 0), -- Warlock
+(464, 89, 89, 0, 0, NULL, NULL, 0), -- Priest
+(465, 156, 156, 0, 0, NULL, NULL, 0), -- Paladin
+(466, 87, 87, 0, 0, NULL, NULL, 0), -- Druid
+(467, 86, 86, 0, 0, NULL, NULL, 0), -- Shaman
+-- Races
+(1404, 113, 113, 0, 0, NULL, NULL, 0), -- Gnome
+(1405, 110, 110, 0, 0, NULL, NULL, 0), -- Blood Elf
+(1406, 111, 111, 0, 0, NULL, NULL, 0), -- Draenei
+(1407, 112, 112, 0, 0, NULL, NULL, 0), -- Dwarf
+(1408, 114, 114, 0, 0, NULL, NULL, 0), -- Human
+(1409, 115, 115, 0, 0, NULL, NULL, 0), -- Night Elf
+(1410, 116, 116, 0, 0, NULL, NULL, 0), -- Orc
+(1411, 117, 117, 0, 0, NULL, NULL, 0), -- Tauren
+(1412, 118, 118, 0, 0, NULL, NULL, 0), -- Troll
+(1413, 119, 119, 0, 0, NULL, NULL, 0), -- Undead
+-- Professions
+(1414, 97, 97, 0, 0, NULL, NULL, 0), -- Blacksmithing
+(1415, 96, 96, 0, 0, NULL, NULL, 0), -- Alchemy
+(1416, 98, 98, 0, 0, NULL, NULL, 0), -- Cooking
+(1417, 99, 99, 0, 0, NULL, NULL, 0), -- Enchanting
+(1418, 100, 100, 0, 0, NULL, NULL, 0), -- Engineering
+(1419, 101, 101, 0, 0, NULL, NULL, 0), -- First Aid
+(1420, 102, 102, 0, 0, NULL, NULL, 0), -- Fishing
+(1421, 103, 103, 0, 0, NULL, NULL, 0), -- Herbalism
+(1422, 104, 104, 0, 0, NULL, NULL, 0), -- Inscription
+(1423, 105, 105, 0, 0, NULL, NULL, 0), -- Jewelcrafting
+(1424, 106, 106, 0, 0, NULL, NULL, 0), -- Leatherworking
+(1425, 107, 107, 0, 0, NULL, NULL, 0), -- Mining
+(1426, 108, 108, 0, 0, NULL, NULL, 0), -- Skinning
+(1427, 109, 109, 0, 0, NULL, NULL, 0); -- Tailoring
